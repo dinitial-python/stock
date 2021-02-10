183b5fb8014a3c4073995c31fba5a0160b909b9f
Daniel.Dong <dinitial520@outlook.com>
2021-02-11 01:37:55 +0800

    Update hongkong.py

--- ggu/hongkong.py
+++ ggu/hongkong.py
@@ -6,7 +6,13 @@ import akshare as ak
 import linecache
 # write excel
 import xlwt
+# read excel
+import xlrd
+# for line number
+import sys
 
+def info(info):
+    print(str(sys._getframe().f_lineno) + info)
 
 # get the line content
 def get_line_context(file_path, line_number):
@@ -28,11 +34,7 @@ def get_line_context(file_path, line_number):
 def get_average_result(str, days):
     # get line number
     cnt = len(open(str, 'r').readlines())
-    #print ("lines numbers: ", cnt)
-
-    # get the line from index value
-    #print("cnt is: ", cnt)
-    #print("days is: ", days)
+    
     good_lines = cnt - 2    
     if good_lines <= days:
         return 0    
@@ -44,14 +46,62 @@ def get_average_result(str, days):
 
     while index <= cnt:
         line = get_line_context(str, index)
-        item4 = line.split()[4]
-        #print(item4)
-        total += float(item4)        
+        item4 = line.split()[4]        
+        total += float(item4)
         index += 1
 
     average = total / days
 
     if float(item4) >= average:
+        return 1
+    else:
+        return 0
+
+#
+# 该函数返回股票代码的当前价格是否为均线的最高价格（均线天数使用 days 参数传入函数）
+#
+# input parameters:
+#   @str : txt filename, include the stock code's price
+#   @days: average days, the value is: 15, 30, 60, etc
+#
+# return value:
+#   if current price biggest, return 1, or will return 0
+#
+# sample:
+#   ret = get_max_result("API.txt", 5)
+#
+def get_max_result(str, days):
+    # get line number
+    cnt = len(open(str, 'r').readlines())
+
+    good_lines = cnt - 2    
+    if good_lines <= days:
+        return 0
+
+    index = cnt - days + 1        
+
+    # get the last day close price
+    line = get_line_context(str, cnt)
+    lastVal = line.split()[4]
+
+    # 获取成交量，用于计算成交额。对于美股，成交额需要大于 1 亿元美金
+    lastMount = line.split()[5]
+    lastMoney = float(lastVal) * float(lastMount)
+    if lastMoney < 100000000:
+        return 0
+
+    total = 0
+
+    while index <= cnt:
+        line = get_line_context(str, index)
+        item4 = line.split()[4]
+
+        if float(item4) < float(lastVal):
+            total += 1
+
+        index += 1
+
+    if total >= (days - 2):
         return 1
     else:
         return 0
@@ -67,16 +117,22 @@ def get_average_result(str, days):
 #   get_stock_price_list("09923", "new_09923.txt")
 #
 def get_stock_price_list(stock, file):
+
+    print("daniel.dong - 1")
+    print(stock)
+    print(file)
+
     # show all the row and col
     pd.set_option('display.max_columns',None)
     pd.set_option('display.max_rows',None)
 
     # open file and store the stock's price list into the file
     price_obj = open(file, mode = 'w',encoding='utf-8')
-    #stock_hk_daily_hfq_df = ak.stock_hk_daily(symbol="09923", adjust="hfq")
     stock_hk_daily_hfq_df = ak.stock_hk_daily(symbol=stock, adjust="hfq")
     print(stock_hk_daily_hfq_df, file=price_obj)
     price_obj.close()
+
+    print("daniel.dong - 2")
 
 #
 # 该函数获取在香港上市的股票列表
@@ -99,7 +155,7 @@ def get_hongkong_stock_list(file):
     stock_obj.close()
 
 
-# Step.1 use get_hongkong_stock_list("hongkong.txt") get the stock list and remove the price information
+# Step.1 get hongkong.txt from futu gonggu page
 # Step.2 as the follow
 
 file = open('hongkong.txt','r', encoding='UTF-8')
@@ -116,10 +172,16 @@ while True:
         break
 
     # 获取股票代码
-    item1 = line.split()[1]
-    price_list = 'test\\' + item1 + '.txt'
-    #price_list = item1 + '.txt'
-    # print("price list file name is: ", price_list)
+    item1 = line.split()[0]
+    print(line)
+   # print(item1)
+
+    # Linux platform
+    # price_list = 'test/' + item1 + '.txt'
+
+    # Windows platform
+    price_list = 'test\\' + item1 + '.txt'       
+    print(price_list)
 
     # 获取指定股票的价格列表，并将其写入到指定文件中
     get_stock_price_list(item1, price_list)
@@ -129,30 +191,26 @@ while True:
     ret60 = get_average_result(price_list, 60)
     ret120 = get_average_result(price_list, 120)
 
-    if ret20 and ret60 and ret120:
+    ret30 = get_average_result(price_list, 30)
+    max30 = get_max_result(price_list, 30)
+
+    if ret30 and max30:
+
+    #if ret20 and ret60 and ret120:
+    #if ret60:
         s_item0 = line.split()[0]
         s_item1 = line.split()[1]
-        s_item2 = line.split()[2]
+        #s_item2 = line.split()[2]
 
         ws.write(raw, 0, s_item0)
         ws.write(raw, 1, s_item1)
-        ws.write(raw, 2, s_item2)
+        #ws.write(raw, 2, s_item2)
+        wb.save('./ggu_20210211.xls')
+
         raw += 1
 
+        print(item1)
+
 print("write finished")
-wb.save('./stock_20210122.xls')
 
-'''
-    if ret20 and ret60 and ret120:
-        print(line)
-'''
-
-#get_average_result("09923.txt", 60)
-''' get_average_result test
-ret = get_average_result("09923.txt", 5)
-if ret:
-    print("current price is bigger")
-else:
-    print("current price is lower")
-'''
 
